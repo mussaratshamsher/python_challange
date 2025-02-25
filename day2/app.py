ae0907441d23063ac8746d86b1cbf41897dd42dd
mussaratshamsher <musaratskhan@github.com>
2025-02-25 08:01:24 +0500

    day2

--- day2/app.py
+++ day2/app.py
@@ -1,15 +1,15 @@
 
 
-# User se input lena  
+# User input 
 user_sentence = input("Write down your sentence: ")  
 
-# Sentence mein words ko count karna  
+# wirds in Sentence  
 word_count = len(user_sentence.split())  
 
-# Words ko reverse order mein arrange karna  
+# Arranging Words into reverse order 
 reversed_words = ' '.join(user_sentence.split()[::-1])  
 
-# Count aur reversed words ko print karna  
+# to print Count and reversed words 
 print(f"Your sentence has total {word_count} words.")  
 print("your message in reverse order:")  
 print(reversed_words)
