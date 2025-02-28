889a862c02cdbf26cfa62be0133ba060b2717dd1
mussaratshamsher <musaratskhan@github.com>
2025-02-28 07:04:17 +0500

    day5

---
+++ day5/app.py
@@ -0,0 +1,7 @@
+
+# write a Python program to calculate the sum of numbers from 1 to a given number.
+
+# Solution: Using sum() and range()
+number = int(input("✏🖋Enter a number: "))
+sum = sum(range(1, number + 1))
+print(f"🌌Sum: ♨➡ {sum}")
