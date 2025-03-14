ee1e933c32e17c85d573385aeadc15826c973ad6
mussaratshamsher <musaratskhan@github.com>
2025-03-14 10:57:22 +0500

    edit

--- day19/app.py
+++ day19/app.py
@@ -46,7 +46,7 @@ def main():
     if uploaded_file is not None:
         # Open the uploaded image
         image = Image.open(uploaded_file)
-        st.image(image, caption="Uploaded Image", use_column_width=True)
+        st.image(image, caption="Uploaded Image", use_container_width=True)
 
         # Convert image to ASCII art
         ascii_art = image_to_ascii(image)
